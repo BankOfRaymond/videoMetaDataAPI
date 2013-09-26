cdc5cd65efef5ce61fd1a083a0629fbab2b89e7e
cyrano821 <cyrano821@eveCamfusionAPI.(none)>
2013-09-26 16:31:23 -0700

    http requests seem to be disabled

--- eve/settings.py
+++ eve/settings.py
@@ -24,7 +24,94 @@ PAGINATION = True
 DATE_FORMAT = "%Y-%M-%d"
 
 
+
+
 schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/nicolaiarocci/cerberus) for details.
+    # 'upload_date': {
+    #     'type': 'datetime'
+    # },
+    # 'release_date': {
+    #     'type': 'datetime'
+    # },
+    # 'brand': {
+    #     'type': 'string',
+    #     'minlength': 1,
+    #     'maxlength': 15,
+    #     # 'required': True
+    # },
+    'video_id': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15,
+        'required': True,
+        # talk about hard constraints! For the purpose of the demo
+        # 'lastname' is an API entry-point, so we need it to be unique.
+        'unique': True,
+    },
+    # # 'role' is a list, and can only contain values from 'allowed'.
+    # 'role': {
+    #     'type': 'list',
+    #     'allowed': ["author", "contributor", "copy"],
+    # },
+    # # An embedded 'strongly-typed' dictionary.
+    # 'location': {
+    #     'type': 'dict',
+    #     'schema': {
+    #         'address': {'type': 'string'},
+    #         'city': {'type': 'string'}
+    #     },
+    # },
+    # 'born': {
+    #     'type': 'datetime',
+    # },
+}
+
+video = {
+    # 'title' tag used in item links. Defaults to the resource title minus
+    # the final, plural 's' (works fine in most cases but not for 'people')
+    'item_title': 'video',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>/'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform
+    # GET requests at '/people/<lastname>/'.
+    'additional_lookup': {
+        'url': '[\w]+',
+        'field': 'video_id'
+    },
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST', 'DELETE'],
+   
+     
+    'schema': schema
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+schema2 = {
     # Schema definition, based on Cerberus grammar. Check the Cerberus project
     # (https://github.com/nicolaiarocci/cerberus) for details.
     'firstname': {
@@ -81,11 +168,12 @@ people = {
     # most global settings can be overridden at resource level
     #'resource_methods': ['GET', 'POST'],
      
-    'schema': schema
+    'schema': schema2
 }
 
 
 DOMAIN = {
     'people': people,
+    'video' : video,
 
 }
